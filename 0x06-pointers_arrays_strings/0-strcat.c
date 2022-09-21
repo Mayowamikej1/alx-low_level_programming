34fd0d6ab62be7d6d1ad0972149d0e13edb21bae
Mayowamikej1 <showmayowa@gmail.com>
2022-09-21 12:25:11 -0700

    Task o

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,7 +4,7 @@
  *  * _strlen - function that return the lenght of a string
  *   * @s: the string
  *    * Return: length of s
- */
+ *     */
 
 int _strlen(char *s)
 {
@@ -13,32 +13,28 @@ int _strlen(char *s)
 	i = 0;
 	while (*(s + i))
 	i++;
-
 	return (i);
 }
 
 /**
- *  * _strncat - function that concatenates two strings
+ *  * _strcat - function that concatenates two strings
  *   * @dest: first string
  *    * @src: second string
- *     * @n: bytes from src the function will use
- *      * Return: a pointer to the resulting string dest
+ *     * Return: a pointer to the resulting string dest
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int ls, ld;
 
 	ld = _strlen(dest);
 	ls = _strlen(src) - 1;
-	if (n < ls)
-	ls = n;
 
 	while (ls >= 0)
 {
 	*(dest + ld + ls) = *(src + ls);
 	ls--;
-}
+													}
 	*(dest + _strlen(dest)) = '\0';
 	return (dest);
 }
